438bfea01243d707e689937ff080b33eeb00ed83
propa-surf <uwe.foditsch@gmail.com>
2022-12-19 13:46:34 +0100

    most of the coingecko API stuff done for adding the coin, firebase related stuff next

--- src/components/PortfolioAddCoinContainer.js
+++ src/components/PortfolioAddCoinContainer.js
@@ -8,10 +8,15 @@ function PortfolioAddCoinContainer({allCoins}) {
     const[query, setQuery] = useState('')
     const[searchedCoin,setSearchedCoin] = useState('')
     const[chosenCoin,setChosenCoin] = useState('')
+    const[amountCoin, setAmountCoin] = useState(0)
 
     const handleSearch=(e)=>{
         setQuery(e.target.value)
         setSearchedCoin(allCoins.filter(coin => coin.name.includes(`${query}`)))
+    }
+
+    const handleAmount=(e)=>{
+        setAmountCoin(e.target.value)
     }
 
   return (
@@ -21,7 +26,7 @@ function PortfolioAddCoinContainer({allCoins}) {
         <div className='portfolio-search-coin'>
             <div className='portfolio-search-coin-search-field'>
                 <label htmlFor='coin-search-field' className='coin-search-field'>Coin:</label>
-                <input type='text' id='coin-search-field' placeholder='Search Field' onChange={handleSearch} required/>
+                <input type='text' id='coin-search-field' placeholder='Coin Name' onChange={handleSearch} required/>
                 {
                     query!==''
                     ? <div className='coin-search-results'>
@@ -37,23 +42,23 @@ function PortfolioAddCoinContainer({allCoins}) {
             <div className='portfolio-search-coin-stats'>
                 <div className='portfolio-search-coin-price'>
                     <p className='coin-current-price'>Current Price</p>
-                    <p className='coin-current-price-display'>{chosenCoin.current_price}</p>
+                    <p className='coin-current-price-display'>${chosenCoin.current_price}</p>
                 </div>
                 <div className='portfolio-search-coin-mcap'>
                     <p className='coin-current-mcap'>Current MCap</p>
-                    <p className='coin-current-mcap-display'>{chosenCoin.market_cap}</p>
+                    <p className='coin-current-mcap-display'>${(chosenCoin.market_cap/1000000000).toFixed(2)} Billion</p>
                 </div>
             </div>
         </div>
         <div className='portfolio-search-add-field'>
             <label htmlFor='coin-add-field' className='coin-add-field'>Add Amount:</label>
-            <input type='number' id='coin-add-field' placeholder='Amount' min='0.000001' required/>
+            <input type='number' id='coin-add-field' placeholder='Amount' min='0.000001' onChange={handleAmount}required/>
         </div>
         <div className='portfolio-search-summary'>
             <p className='summary-position'>Summary Position</p>
-            <p className='summary-coin'>Coin: + Placeholder API</p>
-            <p className='summary-amount'>Amount: + Placeholder API</p>
-            <p className='summary-worth'>Worth: + Placeholder Amount*Price</p>
+            <p className='summary-coin'>Coin: {chosenCoin.name}</p>
+            <p className='summary-amount'>Amount: {amountCoin}</p>
+            <p className='summary-worth'>Worth: ${parseFloat(amountCoin)*parseFloat(chosenCoin.current_price)}</p>
         </div>
         <button className='add-btn'>Add</button>
     </form>

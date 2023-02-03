4257ba607558c85fc9e3bb131c0eb9a71a98552e
propa-surf <uwe.foditsch@gmail.com>
2023-02-03 21:38:37 +0100

    env file added

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -7,12 +7,12 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAKZSxzdTCVIW9Fx0wH6gJ-97Y290N9Q14",
-  authDomain: "coin-portfolio-3a93e.firebaseapp.com",
-  projectId: "coin-portfolio-3a93e",
-  storageBucket: "coin-portfolio-3a93e.appspot.com",
-  messagingSenderId: "966288985816",
-  appId: "1:966288985816:web:52e27854fe7fe51b9d11ad"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
